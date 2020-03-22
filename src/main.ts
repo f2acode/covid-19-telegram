00e3d2d6b0455292f9cb364a31271862d6a8fd3f
Felipe Augusto <augustto.felipe@hotmail.com>
2020-03-22 18:18:50 -0300

    get data

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import * as core from '@actions/core'
+import fetch from 'node-fetch'
 
 async function run(): Promise<void> {
   try {
     core.debug('OLÁAAAAAAA')
+
+    const res = await fetch(
+      'https://pomber.github.io/covid19/timeseries.json',
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+
+    const text = await res.text()
+
+    core.debug(text)
 
     core.setOutput('numberOfCases', '1')
   } catch (error) {
