d68a130d03a89921caf4bedd4341435cc1a7d2f5
Felipe Augusto <augustto.felipe@hotmail.com>
2020-03-22 18:10:04 -0300

    output variable

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import * as core from '@actions/core'
 async function run(): Promise<void> {
   try {
     core.debug('OLÁAAAAAAA')
+
+    core.setOutput('numberOfCases', '1')
   } catch (error) {
     core.setFailed(error.message)
   }
