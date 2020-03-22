d39d04e55f49fb1cb108bb6719302d30a034859e
Felipe Augusto <augustto.felipe@hotmail.com>
2020-03-22 18:10:21 -0300

    fix log

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    console.log('TESTANDO')
     core.debug('OLÁAAAAAAA')
 
     core.setOutput('numberOfCases', '1')
