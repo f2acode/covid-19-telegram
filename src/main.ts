8bb339f29c9d2dba165338bc859f4fb2624f00db
Felipe augusto <augustto.felipe@hotmail.com>
2020-03-22 17:11:47 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
+    console.log('TESTANDO')
     core.debug('OLÁAAAAAAA')
   } catch (error) {
     core.setFailed(error.message)
